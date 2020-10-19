841fe4c2e5a9eddb993c16f5a96e3dd311b6bedb
Sabri Kargı <sabri_16002@hotmail.com>
2020-10-19 17:11:52 +0300

    İsim

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //deneme
+            //deneme İsmail Sabri Kargı
         }
     }
 }
